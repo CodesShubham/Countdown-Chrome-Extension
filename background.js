f4aad541c19eaf61cc325ce3e1ef6535cbabca60
69PercentFat <shubham11501@gmail.com>
2023-10-01 18:20:36 +0530

    Version 2.0
    updated js for smooth operation

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-let countdownInterval;
+// background.js
 
-function startCountdown(targetDateTime) {
+let countdownInterval;
+let targetDateTime;
+
+// Function to send a message to the popup to update the countdown display
+function updatePopupCountdown(countdownText) {
+    chrome.runtime.sendMessage({ action: "updateCountdown", countdownText: countdownText });
+}
+
+function startCountdown() {
     clearInterval(countdownInterval);
 
     countdownInterval = setInterval(function () {
@@ -17,14 +25,34 @@ function startCountdown(targetDateTime) {
             const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
             const countdownText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-            chrome.action.setBadgeText({ text: countdownText });
+            updatePopupCountdown(countdownText); // Send the countdown text to the popup
         }
     }, 1000);
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "startCountdown") {
-        const targetDateTime = new Date(request.dateTime);
-        startCountdown(targetDateTime);
+        targetDateTime = new Date(request.dateTime);
+        startCountdown();
+    } else if (request.action === "getCountdown") {
+        const currentDate = new Date();
+        const timeDifference = targetDateTime - currentDate;
+
+        if (timeDifference > 0) {
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+            const countdownText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+            sendResponse({ countdownText: countdownText });
+        } else {
+            sendResponse({ countdownText: "Countdown expired!" });
+        }
     }
+});
+
+// Initialize the extension
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.action.setBadgeText({ text: "" });
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
+// popup.js
+
 document.getElementById('startButton').addEventListener('click', function () {
     const dateTime = document.getElementById('datetime').value;
 
+    // Send a message to background.js to start the countdown
     chrome.runtime.sendMessage({ action: "startCountdown", dateTime: dateTime });
+});
+
+// Listen for messages from background.js to update the countdown display
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "updateCountdown") {
+        const countdownText = request.countdownText;
+        document.getElementById('countdown').textContent = countdownText;
+    }
+});
+
+// Request initial countdown when popup is opened
+chrome.runtime.sendMessage({ action: "getCountdown" }, function (response) {
+    if (response && response.countdownText) {
+        document.getElementById('countdown').textContent = response.countdownText;
+    }
 });
